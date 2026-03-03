b656883320e8c83d4ac4d473ef2abb618aa68c2a
Wellython <wellython.dev@gmail.com>
2026-03-03 18:17:49 -0400

    test: implement frontend unit tests for Login security using Vitest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+  test: {
+    globals: true,
+    environment: 'jsdom', // Finge ser um navegador no terminal
   }
 })
